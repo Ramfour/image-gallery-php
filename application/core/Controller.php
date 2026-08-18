e3f745f4760f2945868aa2dfd320c38c0692cc89
Ramfour <bdddanil2002@gmail.com>
2026-08-18 15:16:33 +0700

    feat: add \ to base Controller, fix View::generate() to pass data via extract()

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -3,9 +3,11 @@ class Controller
 {
     public $model;
     public $view;
+    protected $pdo;
 
     function __construct()
     {
         $this->view = new View();
+        $this->pdo = require_once 'application/core/db.php';
     }
 }
--- application/core/View.php
+++ application/core/View.php
@@ -3,6 +3,9 @@ class View
 {
 	function generate($content_view, $template_view, $data = null)
 	{
+		if (is_array($data)) {
+			extract($data); // превращает ['error' => '...'] в $error
+		}
 		include 'application/views/'.$template_view;
 	}
 }
