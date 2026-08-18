c0caecc3125372800e1e9fb23eba05132cbacffe
Ramfour <bdddanil2002@gmail.com>
2026-08-18 15:17:56 +0700

    chore: add empty skeleton files for gallery and comments

---
+++ application/controllers/controller_gallery.php
@@ -0,0 +1,7 @@
+<?php
+/** @see Controller */
+class Controller_Contacts extends Controller {
+    function action_index() {
+        $this->view->generate('contacts_view.php', 'template_view.php');
+    }
+}
