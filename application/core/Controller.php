9dd04bcc1a5fcfc08a0d9e7ee49177dda8d4d076
Ramfour <bdddanil2002@gmail.com>
2026-08-18 17:06:16 +0700

    fix: get PDO from globals instead of require_once (avoid bool return on second include)

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -8,7 +8,7 @@ class Controller
     function __construct()
     {
         $this->view = new View();
-        $this->pdo = require_once $_SERVER['DOCUMENT_ROOT'] . '/application/core/db.php';
+        $this->pdo = $GLOBALS['pdo'];
     }
 
     protected function requireAuth()
