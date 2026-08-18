e8075e364880738ff732c8ff420d9aa0bbf677e6
Ramfour <bdddanil2002@gmail.com>
2026-08-18 17:04:35 +0700

    fix: correct case-sensitive filenames for Linux (Model/View/Controller)

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -2,8 +2,8 @@
 session_start();
 require_once __DIR__ . '/../config.php';
 $pdo = require_once __DIR__ . '/core/db.php';
-require_once __DIR__ . '/core/model.php';
-require_once __DIR__ . '/core/view.php';
-require_once __DIR__ . '/core/controller.php';
+require_once __DIR__ . '/core/Model.php';
+require_once __DIR__ . '/core/View.php';
+require_once __DIR__ . '/core/Controller.php';
 require_once __DIR__ . '/core/route.php';
 Route::start(); // запускаем маршрутизатор
