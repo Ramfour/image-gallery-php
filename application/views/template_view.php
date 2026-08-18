897c699377a4836e54dd5ca59d8eccfa7015af25
Ramfour <bdddanil2002@gmail.com>
2026-08-18 15:47:31 +0700

    feat: Bootstrap 5 layout, navbar with auth state, updated main page

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -1,19 +1,59 @@
 <?php /** @var string $content_view */ ?>
 <!DOCTYPE html>
 <html lang="ru">
-
 <head>
     <meta charset="utf-8">
-    <title>Главная</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Image Gallery</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous">
 </head>
+<body class="bg-light">
 
-<body>
-<nav>
-    <a href="/">Главная</a> |
-    <a href="/?url=about">О нас</a> |
-    <a href="/?url=contacts">Контакты</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+    <div class="container">
+        <a class="navbar-brand" href="/">Image Gallery</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/?url=gallery">Галерея</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <?php if (!empty($_SESSION['user_id'])): ?>
+                    <li class="nav-item">
+                        <span class="nav-link text-light">
+                            <?= htmlspecialchars($_SESSION['login']) ?>
+                        </span>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/?url=gallery/upload">Загрузить фото</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/?url=auth/logout">Выйти</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/?url=auth">Войти</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/?url=auth/register">Регистрация</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
 </nav>
-<hr>
-<?php include 'application/views/' . $content_view; ?></body>
 
+<?php include 'application/views/' . $content_view; ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc4s9bIOgUxi8T/jzmZ0XkzlnOVt8Hl/w7Nkjg3FVW6"
+        crossorigin="anonymous"></script>
+</body>
 </html>
